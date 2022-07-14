a981a97af3e8c15e59d07fa788c66b0604d045e4
aiviaghost <smurfplatta@gmail.com>
2022-07-15 00:18:22 +0200

    Add matrix addition

--- SkalaNet/src/main/scala/Matrix.scala
+++ SkalaNet/src/main/scala/Matrix.scala
@@ -1,6 +1,6 @@
 package SkalaNet
 
-case class Matrix private (private val M: Array[Array[Float]]):
+case class Matrix (private val M: Array[Array[Float]]):
     import scalanative.unsafe.*   
     
     private val (rows, cols) = (M.size, M(0).size)
@@ -12,18 +12,28 @@ case class Matrix private (private val M: Array[Array[Float]]):
         res: Ptr[CFloat]
     ): Unit = extern
 
+    def +(other: Matrix): Matrix = 
+        assert(rows == other.rows && cols == other.cols, "Matrix dimensions do not match!")
+
+        val newM = Array.fill(rows)(Array.ofDim[Float](cols))
+        for i <- 0 until rows do 
+            for j <- 0 until cols do
+                newM(i)(j) = M(i)(j) + other.M(i)(j)
+        
+        Matrix(newM)
+
     def *(other: Matrix): Matrix = 
         assert(cols == other.rows, "Dimensions are not valid for multiplication!")
         
         val (n, m, p) = (rows, cols, other.cols)
         
         val A = stackalloc[CFloat](n * m)
-        for i <- 0 until n do
+        for i <- 0 until n do 
             for j <- 0 until m do
                 !(A + i * m + j) = M(i)(j)
         
         val B = stackalloc[CFloat](m * p)
-        for i <- 0 until m do
+        for i <- 0 until m do 
             for j <- 0 until p do
                 !(B + i * p + j) = other.M(i)(j)
         
@@ -31,9 +41,10 @@ case class Matrix private (private val M: Array[Array[Float]]):
         mult(n, m, p, A, B, res)
         
         val newM = Array.fill(n)(Array.ofDim[Float](p))
-        for i <- 0 until n do
+        for i <- 0 until n do 
             for j <- 0 until p do
                 newM(i)(j) = !(res + i * p + j)
+        
         Matrix(newM)
 
     override def toString(): String = M.map(_.mkString(", ")).mkString("\n")
