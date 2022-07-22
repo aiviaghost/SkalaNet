47d3e7cee90412fd3a3c368fbe3730b0f0ec290e
Julius Thunström <48349394+aiviaghost@users.noreply.github.com>
2022-07-22 04:13:04 +0200

    Refactor Image toString

--- SkalaNet/src/main/scala/Image.scala
+++ SkalaNet/src/main/scala/Image.scala
@@ -4,10 +4,8 @@ case class Image private (val label: Int, private val pixels: Array[Array[Int]])
     def toColumnVector(): Matrix = ???
 
     override def toString(): String = 
-        import math.floorDiv
         val byte2ascii = """ .'`^",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"""
-        val LEN = byte2ascii.length
-        val digit = pixels.map("|" + _.map(pixel => byte2ascii(floorDiv(pixel * LEN, 256))).mkString + "|").mkString("\n")
+        val digit = pixels.map("|" + _.map(pixel => byte2ascii(pixel * byte2ascii.size / 256)).mkString + "|").mkString("\n")
         val bar = "+" + "-".padTo(28, "-").mkString + "+" // "-".repeat(28) is not supported by Scala Native :(
         Seq(bar, digit, bar).mkString("\n")
 
