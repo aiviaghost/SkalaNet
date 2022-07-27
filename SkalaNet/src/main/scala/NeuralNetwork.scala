6d563ee70fd7dd67fe2938eb699c53ac8193080a
aiviaghost <smurfplatta@gmail.com>
2022-07-28 00:39:29 +0200

    Remove comment and update method name

--- SkalaNet/src/main/scala/NeuralNetwork.scala
+++ SkalaNet/src/main/scala/NeuralNetwork.scala
@@ -1,10 +1,5 @@
 package SkalaNet
 
-//     W      *     X     +    B     =>  X_next
-// (16 x 784) * (784 x 1) + (16 x 1) => (16 x 1)
-// (16 x 16)  *  (16 x 1) + (16 x 1) => (16 x 1)
-// (10 x 16)  *  (16 x 1) + (10 x 1) => (10 x 1)
-
 case class NeuralNetwork (val layers: Int*):
     val layerDimensions = layers.zip(layers.tail)
     val weights = layerDimensions.map((n, m) => Matrix.fillRandom(n, m))
@@ -13,4 +8,4 @@ case class NeuralNetwork (val layers: Int*):
     // ReLU
     private def __/(x: Matrix): Matrix = x.map(math.max(_, 0))
 
-    private def feedForward(inp: Matrix): Matrix = weights.zip(biases).foldLeft(inp){case (x, (w, b)) => __/(w * x + b)}
+    private def feedforward(inp: Matrix): Matrix = weights.zip(biases).foldLeft(inp){case (x, (w, b)) => __/(w * x + b)}
