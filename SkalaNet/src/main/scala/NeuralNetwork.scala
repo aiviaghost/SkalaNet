d79af692a4cc9aed99cf18d7dfe4c86643ca7ee5
aiviaghost <smurfplatta@gmail.com>
2022-07-28 00:48:27 +0200

    Update variable name and access modifiers

--- SkalaNet/src/main/scala/NeuralNetwork.scala
+++ SkalaNet/src/main/scala/NeuralNetwork.scala
@@ -1,11 +1,11 @@
 package SkalaNet
 
-case class NeuralNetwork (val layers: Int*):
-    val layerDimensions = layers.zip(layers.tail)
-    val weights = layerDimensions.map((n, m) => Matrix.fillRandom(n, m))
-    val biases = layerDimensions.map((n, _) => Matrix.fillRandom(n, 1))
+case class NeuralNetwork (private val layers: Int*):
+    private val dimensions = layers.zip(layers.tail)
+    private val weights = dimensions.map((n, m) => Matrix.fillRandom(n, m))
+    private val biases = dimensions.map((n, _) => Matrix.fillRandom(n, 1))
 
-    // ReLU
+    // ReLU ;)
     private def __/(x: Matrix): Matrix = x.map(math.max(_, 0))
 
     private def feedforward(inp: Matrix): Matrix = weights.zip(biases).foldLeft(inp){case (x, (w, b)) => __/(w * x + b)}
