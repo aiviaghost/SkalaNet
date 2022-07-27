0c7c020b5e185b6fff0cca8613373884ca8c13b3
aiviaghost <smurfplatta@gmail.com>
2022-07-27 21:57:48 +0200

    Add link to PR regarding string method "replace"

--- SkalaNet/src/main/scala/Image.scala
+++ SkalaNet/src/main/scala/Image.scala
@@ -20,7 +20,7 @@ object Image:
     def readImages(imageFile: String, labelFile: String): Seq[Image] = 
         val labels = readLabels(labelFile)
         readBytes(imageFile).drop(16)
-                            .map(_.toInt & 255) // convert to unsigned "byte" by masking with 0b11111111
+                            .map(_.toInt & 255) // convert to unsigned "byte" by masking with 0b11111111, https://github.com/scala-native/scala-native/pull/2711
                             .grouped(28 * 28)
                             .map(_.grouped(28).toArray)
                             .zip(labels)
