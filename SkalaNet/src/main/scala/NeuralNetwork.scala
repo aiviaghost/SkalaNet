9caf6d38062d9dbfa446531fb2c0208fca4ab609
aiviaghost <smurfplatta@gmail.com>
2022-08-05 13:49:26 +0200

    Fix NeuralNet outputting 1-10 instead of expected 0-9

--- SkalaNet/src/main/scala/NeuralNetwork.scala
+++ SkalaNet/src/main/scala/NeuralNetwork.scala
@@ -6,7 +6,7 @@ case class NeuralNetwork (private val layers: Int*):
     private val dimensions = layers.tail.zip(layers)
     private val weights = dimensions.map((n, m) => Matrix.fillRandom(n, m))
     private val biases = dimensions.map((n, _) => Matrix.fillRandom(n, 1))
-
+    
     // ReLU ;)
     private def __/(x: Matrix): Matrix = x.mmap(math.max(_, 0))
 
@@ -15,4 +15,4 @@ case class NeuralNetwork (private val layers: Int*):
 
     // query the network using a matrix representing the image
     def apply(inp: Matrix): Int = 
-        feedforward(inp).flatten.zipWithIndex.max._2 + 1
+        feedforward(inp).flatten.zipWithIndex.max._2
