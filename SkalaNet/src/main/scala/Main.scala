677653810b94e6e8a8bb2b31bdb1723595d2b5ff
aiviaghost <smurfplatta@gmail.com>
2022-09-25 04:05:41 +0200

    Change indent.callSite to 4

--- SkalaNet/src/main/scala/Main.scala
+++ SkalaNet/src/main/scala/Main.scala
@@ -1,12 +1,12 @@
 package SkalaNet
 
 lazy val trainingImages = Image.readImages(
-  imageFile = "../MNIST/training_set/train-images-idx3-ubyte",
-  labelFile = "../MNIST/training_set/train-labels-idx1-ubyte"
+    imageFile = "../MNIST/training_set/train-images-idx3-ubyte",
+    labelFile = "../MNIST/training_set/train-labels-idx1-ubyte"
 )
 lazy val testImages = Image.readImages(
-  imageFile = "../MNIST/test_set/t10k-images-idx3-ubyte",
-  labelFile = "../MNIST/test_set/t10k-labels-idx1-ubyte"
+    imageFile = "../MNIST/test_set/t10k-images-idx3-ubyte",
+    labelFile = "../MNIST/test_set/t10k-labels-idx1-ubyte"
 )
 
 val nn = NeuralNetwork.ofDim(784, 16, 16, 10)
@@ -25,16 +25,16 @@ def tryNetwork() =
         val image = images.next()
         println(image)
         println(
-          s"SkalaNet thinks this is the digit ${nn(image.toColumnVector())}."
+            s"SkalaNet thinks this is the digit ${nn(image.toColumnVector())}."
         )
         println(s"The correct answer is ${image.label}.")
         println("Try another image? (y/n)")
         continue = io.StdIn.readLine("Your choice: ") == "y"
 
 def trainNetwork() = nn.SGD(
-  trainingData = trainingImages,
-  epochs = 5,
-  batchSize = 100
+    trainingData = trainingImages,
+    epochs = 5,
+    batchSize = 100
 )
 
 def saveNetwork() = ???
@@ -46,20 +46,20 @@ def help() = ???
 def quit() = System.exit(0)
 
 val menuOptions = Seq(
-  ("Try network", tryNetwork _),
-  ("Score network", scoreNetwork _),
-  ("Train network", trainNetwork _),
-  ("Save network", saveNetwork _),
-  ("Load network", loadNetwork _),
-  ("Help", help _),
-  ("Quit", quit _)
+    ("Try network", tryNetwork _),
+    ("Score network", scoreNetwork _),
+    ("Train network", trainNetwork _),
+    ("Save network", saveNetwork _),
+    ("Load network", loadNetwork _),
+    ("Help", help _),
+    ("Quit", quit _)
 )
 
 def getMenuChoice(): Option[Int] =
     println(
-      menuOptions.zipWithIndex
-          .map((p, i) => s"${i + 1}. ${p._1}")
-          .mkString("\n")
+        menuOptions.zipWithIndex
+            .map((p, i) => s"${i + 1}. ${p._1}")
+            .mkString("\n")
     )
     io.StdIn.readLine("Your choice: ").toIntOption
 
@@ -71,9 +71,9 @@ def getMenuChoice(): Option[Int] =
                 menuOptions(i - 1)._2()
             case Some(_) =>
                 println(
-                  s"That is not a valid option! Please input an integer in the interval [1, ${menuOptions.length}]"
+                    s"That is not a valid option! Please input an integer in the interval [1, ${menuOptions.length}]"
                 )
             case None =>
                 println(
-                  s"That is not an integer! Please input an integer in the interval [1, ${menuOptions.length}]"
+                    s"That is not an integer! Please input an integer in the interval [1, ${menuOptions.length}]"
                 )
