651167c6105587e047f1551788eba09ad7e7d653
aiviaghost <smurfplatta@gmail.com>
2023-01-10 08:22:08 +0100

    Move zip() from utils.scala to NeuralNetwork.scala, delete utils.scala

--- SkalaNet/src/main/scala/NeuralNetwork.scala
+++ SkalaNet/src/main/scala/NeuralNetwork.scala
@@ -1,7 +1,6 @@
 package SkalaNet
 
 import collection.mutable.ArrayBuffer
-import Utils.*
 import util.Random.shuffle
 
 @SerialVersionUID(42L)
@@ -110,6 +109,16 @@ case class NeuralNetwork private (private val layerSizes: Seq[Int])
             deltaW.append(delta * aPrev.transpose)
 
         (deltaW.reverse.toSeq, deltaB.reverse.toSeq)
+
+    private def zip[A, B, C](
+        l1: IterableOnce[A],
+        l2: IterableOnce[B],
+        l3: IterableOnce[C]
+    ): IndexedSeq[(A, B, C)] =
+        val (i1, i2, i3) = (l1.iterator, l2.iterator, l3.iterator)
+        (0 until Seq(l1.size, l2.size, l3.size).max).map(_ =>
+            (i1.next, i2.next, i3.next)
+        )
 
 object NeuralNetwork:
 
